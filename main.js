c48d08349510faec3ebce68ab5cfb217a8e26d65
M Prashanth Rao <92880291+Prashanth123455@users.noreply.github.com>
2022-03-29 18:30:09 +0530

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let loadImage = (src,callback) => {
     img.src = src;
 };
 let imagePath = (frameNumber,animation) =>{
-   return   +"/"+ frameNumber + ".png";
+   return animation  +"/"+ frameNumber + ".png";
 };
 let frames = {
   idle: [1,2,3,4,5,6,7,8],
